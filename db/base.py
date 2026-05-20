9cd7b8d5c332a52dea279896c60c9ee4b409f9f1
zakiy380k <danilkadodi@gmail.com>
2026-05-20 11:00:10 +0300

    Cordianl changes

--- db/base.py
+++ db/base.py
@@ -1,7 +1,13 @@
+import os
+
 from sqlalchemy.ext.asyncio import create_async_engine
 from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "sqlite+aiosqlite:///database.db"
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite+aiosqlite:///database.db")
+
+if DATABASE_URL.startswith("postgresql://") or DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgresql://", "postgresql+asyncpg://", 1)
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql+asyncpg://", 1)
 
 engine = create_async_engine(
     DATABASE_URL,
